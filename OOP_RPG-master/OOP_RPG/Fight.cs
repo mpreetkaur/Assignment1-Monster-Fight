3b504532b17b12f0a2d7211c75f9bc665ee047d6
Manpreet Kaur <mpreet2000kaur@gmail.com>
2018-09-10 17:22:15 -0500

    Initial Commit

---
+++ OOP_RPG-master/OOP_RPG/Fight.cs
@@ -0,0 +1,110 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OOP_RPG
+{
+    public class Fight
+    {
+        List<Monster> Monsters { get; set; }
+        public Game game { get; set; }
+        public Hero hero { get; set; }
+        public Monster monster { get; set; }
+        
+        public Fight(Hero hero, Game game) {
+            this.Monsters = new List<Monster>();
+            this.hero = hero;
+            this.game = game;
+            this.AddMonster("Squid", 9, 8, 20);
+            this.AddMonster("Ninja", 13, 15, 40);
+            this.AddMonster("Pikachu", 5, 17, 10);
+            this.AddMonster("Turtle", 9, 21, 35);
+            var enemy = this.Monsters[0];
+            var lastEnemy = this.Monsters.Last();
+            var secondEnemy = this.Monsters.ElementAt(1);
+            var randomEnemy = this.Monsters.OrderBy(p => Guid.NewGuid()).LastOrDefault();
+            var lessHpEnemy = (from p in this.Monsters select p.CurrentHP < 20).FirstOrDefault();
+            var lessStrengthEnemy = (from p in this.Monsters select p.Strength >= 11).FirstOrDefault();
+            this.monster = randomEnemy;
+
+        }
+
+        public void AddMonster(string name, int strength, int defense, int hp) {
+            var monster = new Monster(name, strength, defense,hp,hp);
+            this.Monsters.Add(monster);
+        }
+        
+        public void Start() {
+           
+
+            Console.WriteLine("You've encountered a " + monster.Name + "! " + monster.Strength + " Strength/" + monster.Defense + " Defense/" +
+            monster.CurrentHP + " HP. What will you do?");
+            Console.WriteLine("1. Fight");
+            var input = Console.ReadLine();
+            if (input == "1") {
+                this.HeroTurn();
+            }
+            else { 
+                this.game.Main();
+            }
+        }
+        
+        public void HeroTurn(){
+          
+           var compare = hero.Strength - monster.Defense;
+           int damage;
+           
+           if(compare <= 0) {
+               damage = 1;
+                monster.CurrentHP -= damage;
+           }
+           else{
+               damage = compare;
+                monster.CurrentHP -= damage;
+           }
+           Console.WriteLine("You did " + damage + " damage!");
+           
+           if(monster.CurrentHP <= 0){
+               this.Win();
+           }
+           else
+           {
+               this.MonsterTurn();
+           }
+           
+        }
+        
+        public void MonsterTurn(){
+           int damage;
+           var compare = monster.Strength - hero.Defense;
+           if(compare <= 0) {
+               damage = 1;
+               hero.CurrentHP -= damage;
+           }
+           else{
+               damage = compare;
+               hero.CurrentHP -= damage;
+           }
+           Console.WriteLine(monster.Name + " does " + damage + " damage!");
+           if(hero.CurrentHP <= 0){
+               this.Lose();
+           }
+           else
+           {
+               this.Start();
+           }
+        }
+        
+        public void Win() {
+            Console.WriteLine(monster.Name + " has been defeated! You win the battle!");
+            game.Main();
+        }
+        
+        public void Lose() {
+            Console.WriteLine("You've been defeated! :( GAME OVER.");
+            return;
+        }
+        
+    }
+    
+}
---
+++ OOP_RPG-master/OOP_RPG/Monster.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+
+namespace OOP_RPG
+{
+    public class Monster
+    {
+        public string Name { get; set; }
+        public int Strength { get; set; }
+        public int Defense { get; set; }
+        public int OriginalHP { get; set; }
+        public int CurrentHP { get; set; }
+
+        public Monster(string name, int strength, int defense, int originalHP , int currentHP) {
+            this.Name = name;
+            this.Strength = strength;
+            this.Defense = defense;
+            this.OriginalHP = originalHP;
+            this.CurrentHP = currentHP;
+        }
+    }
+}
